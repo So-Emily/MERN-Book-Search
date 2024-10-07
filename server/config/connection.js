925503345fbda2b77fc5ee2a99f11a0c69603b06
Emily Soriano <esoriano110@gmail.com>
2024-10-07 13:15:09 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,6 @@ const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/g
 
 mongoose.connect(connectionString, {
     ssl: true,
-    sslValidate: true,
 });
 
 module.exports = mongoose.connection;
