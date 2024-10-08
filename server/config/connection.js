8f078256a72c66ee7515adcfe5d8cc9e459a97cf
Emily Soriano <esoriano110@gmail.com>
2024-10-07 17:50:29 -0700

    changing connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
 mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     ssl: true,
 });
 
